04ee2542aae994bef6e50690ab2f56f782556220
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-11-16 00:53:48 +0000

    student functionality fixed

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,6 @@ Rails.application.routes.draw do
 
   post("/insert_student", { :controller => "students", :action => "create" })
 
-  get("/students", { :controller => "students", :action => "index" })
   
   post("/modify_student/:path_id", { :controller => "students", :action => "update" })
 
